06e0f2c571a6245c7fdacad4ed44cce7458983b4
SSARCandy <ssarcandy@gmail.com>
2016-04-05 21:33:05 +0800

    navbar

--- static/js/share.js
+++ static/js/share.js
@@ -1,9 +1,11 @@
 function share() {
-	window.open('https://www.facebook.com/dialog/share?%20app_id=113786725672316&display=popup&href=' + window.location.href, '分享','width=600,height=400');
+	window.open('https://www.facebook.com/dialog/share?%20app_id=113786725672316&display=popup&href=' + window.location.href, '分享', 'width=600,height=400');
 }
 
-$(document).ready(function(e){
-	if($(window).width() <= 768) {
+$(document).ready(function(e) {
+	if ($(window).width() <= 768) {
 		$('.job-p').css('height', $(window).innerHeight());
 	}
+	var container = document.getElementById('body');
+	Ps.initialize(container);
 });
