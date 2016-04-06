526cf18a4ccb9741cc56fdb7730244b15c7827b9
ssarcandy <ssarcandy@gmail.com>
2016-04-06 14:06:20 +0800

    fbshare

--- static/js/share.js
+++ static/js/share.js
@@ -1,5 +1,5 @@
 function share() {
-	window.open('https://www.facebook.com/dialog/share?%20app_id=113786725672316&display=popup&href=' + window.location.href, '分享', 'width=600,height=400');
+	window.open('https://www.facebook.com/dialog/share?%20app_id=256435891359679&display=popup&href=' + window.location.href, '分享', 'width=600,height=400');
 }
 
 $(document).ready(function(e) {
