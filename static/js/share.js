3aa9ed74e3db533cc16c04f3c573105d4265b74f
rob0215 <101703044@nccu.edu.tw>
2016-04-21 17:32:13 +0800

    home button

--- static/js/share.js
+++ static/js/share.js
@@ -2,19 +2,19 @@ function share() {
 	window.open('https://www.facebook.com/dialog/share?%20app_id=256435891359679&display=popup&href=' + window.location.href, '分享', 'width=600,height=400');
 }
 
-$(document).ready(function (e) {
-	$(".nav li").on("click", function () {
+$(document).ready(function(e) {
+	$(".nav li").on("click", function() {
 		$(".nav li").removeClass("active");
 		$(this).addClass("active");
 	});
 	// mobile hamburger bar
 	$(".button-collapse").sideNav();
 	// slider initialize
-		// if ($(window).height() < 700) {
-		// 	$('.slider').slider({ full_width: true, interval: 8000, height: $(window).height() - 110 });
-		// } else {
-		// 	$('.slider').slider({ full_width: true, interval: 8000, height: 550 });
-		// }
+	// if ($(window).height() < 700) {
+	// 	$('.slider').slider({ full_width: true, interval: 8000, height: $(window).height() - 110 });
+	// } else {
+	// 	$('.slider').slider({ full_width: true, interval: 8000, height: 550 });
+	// }
 	$('.parallax').parallax();
 	$('.carousel').carousel({ dist: -10, padding: -20, shift: 0 });
 	$('.materialboxed').materialbox();
@@ -24,14 +24,21 @@ $(document).ready(function (e) {
 		rowHeight: 100,
 		rel: 'gallery2',
 		margins: 1
-	}).on('jg.complete', function () {
+	}).on('jg.complete', function() {
 		$('#gallery a').swipebox();
 	});
 
-  $('.slider')
+	$('.slider')
 		.slider({
 			full_width: true,
 			height: '101%',
+			indicators: false,
+			// interval: 10000,
+		});
+	$('.slider.home')
+		.slider({
+			full_width: true,
+			height: $(window).height() -  64,
 			indicators: false,
 			// interval: 10000,
 		});
